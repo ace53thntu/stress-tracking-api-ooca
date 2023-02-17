b74f30334dffefd7abf1fe7ccf05299c4adff7df
Em Dinh <ace53thntu@gmail.com>
2023-02-17 18:04:13 +0700

    setup docker for postgres, connect to db by typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,34 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SnakeNamingStrategy } from './utils/snake-naming.strategy';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: +configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          namingStrategy: new SnakeNamingStrategy(),
+          migrationsRun: true,
+          logging: configService.get('DB_LOGS') === 'true' ? true : false,
+          entities: ['./modules/**/*.entity{.ts,.js}'],
+          migrations: ['./database/migrations/*{.ts,.js}'],
+        };
+      },
     }),
   ],
   controllers: [AppController],
